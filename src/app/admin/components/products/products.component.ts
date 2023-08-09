e7bc7203bd6fa63442b639671462ca53c08a9942
OsmancanY <76731718+OsmancanY@users.noreply.github.com>
2023-08-09 17:30:32 +0300

    #

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Product } from 'src/app/contracts/product';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -11,13 +13,21 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   /**
    *
    */
-  constructor(spinner : NgxSpinnerService) {
+  constructor(spinner : NgxSpinnerService,private httpClientService:HttpClientService) {
     super(spinner);
     
   }
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.Pacman);
+    
+   this.httpClientService.get<Product[]>({controller:"products"}).subscribe()
+
+   /*  this.httpClientService.post({controller:"products"},{name:"BlueTea",stock:"15",price:"125"}).subscribe() 
+
+   this.httpClientService.put({controller:"products"},{id:"f23f6450-abab-4cf4-b6fa-480940466e94",name:"BlueTea",stock:"30",price:"125"}).subscribe()
+
+   this.httpClientService.delete({controller:"products"},{id:"f23f6450-abab-4cf4-b6fa-480940466e94"}).subscribe()*/
   }
 
 }
